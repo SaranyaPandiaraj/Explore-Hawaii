8f62bb959415cc423f129f112611256df98952bd
Madhavinamballa <madhavi@Madhavinamballas-MacBook-Pro.local>
2019-12-15 02:13:13 -0800

    added Tourism dashboard

---
+++ Tourism_Hawii/app.py
@@ -0,0 +1,44 @@
+import os
+import pandas as pd
+import numpy as np
+from flask import Flask, jsonify, render_template
+
+app = Flask(__name__)
+@app.route("/")
+def index():
+    """Return the homepage."""
+    return render_template("index.html")
+
+
+
+@app.route("/stateWide")
+def stateWide():
+    return render_template("stateWide.html")
+@app.route("/tourismoahu")
+def tourismoahu():
+    return render_template("tourismoahu.html")
+@app.route("/tourismkaui")
+def tourismkaui():
+    return render_template("tourismkaui.html")
+@app.route("/tourismmaui")
+def tourismMaui():
+    return render_template("tourismmaui.html")
+@app.route("/tourismmolokai")
+def tourismMolokai():
+    return render_template("tourismmolokai.html")
+@app.route("/tourismLanai")
+def tourismLanai():
+    return render_template("tourismLanai.html")
+@app.route("/tourismHawiiIsland")
+def tourismHawiiIslands():
+    return render_template("tourismHawiiIslands.html")
+@app.route("/tourismMap")
+def tourismMap():
+    return render_template("tourismMaps.html")
+
+
+
+
+
+if __name__ == "__main__":
+    app.run()
